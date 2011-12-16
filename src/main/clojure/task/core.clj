49c1f50f53b577544f9ca96f65835ab356304e7b
mikera <mike.r.anderson.13@gmail.com>
2011-12-16 01:03:04 +0000

    Base task implementation

--- src/main/clojure/task/core.clj
+++ src/main/clojure/task/core.clj
@@ -1,2 +1,225 @@
-(ns task.core)
+(ns task.core
+  (:require [clj-time.core :as time])
+  (:require [clojure.pprint]))
+
+;; ====================================================================================
+;; Task data structure
+
+(defrecord TaskData [])
+
+(defn task* [options function]
+  (TaskData. nil 
+    (merge options
+           {:function function
+            :promise (promise)
+            :creation-time (time/now)})))
+
+
+;; =======================================================================================
+;; Task status
+
+(defonce last-task-counter (atom 0))
+
+(defonce tasks (ref {}))
+
+(defn allocate-task-id []
+  (swap! last-task-counter inc))
+
+;; =====================================================================================
+;; Task query functions
+
+(defn stopped? [task]
+  (or
+    (nil? task)
+    (#{:stopped} (:status task))))
+
+
+(defn complete? [task]
+  (or
+    (nil? task)
+    (#{:complete :error :stopped} (:status task))))
+
+
+(defn get-task 
+  "Returns the current task associated with the given ID.
+
+  If called with an old task instance, returns the latest task for the same ID"
+  [task-or-id] 
+  (let [id (cond 
+						 (associative? task-or-id) (:id task-or-id)
+						 :else task-or-id)
+        task (@tasks id)]
+    task)) 
+
+(def task-summary-fields [:id :status :source :options :result])
+
+(defn task-summary [task]
+  {:id (:id task)
+    :status (:status task)
+    :source (:source task)
+    :options (:options task)
+    :result (:result task)})
+
+
+;; =====================================================================================
+;; Task creation
+
+(defmacro wrap-if [condition form]
+  `(if ~condition
+     (fn [~'code] ~form)
+     identity))
+
+(defn build-task-function 
+  "Create the source code for the function to be called for each task execution. 
+   Code generation is done based on the provided options - you only pay for the 
+   options selected." 
+ 
+  ([{repeat :repeat 
+     sleep :sleep 
+     accumulate :accumulate-results 
+     repeat-value :repeat
+     while-clause :while 
+     until-clause :until  
+     :as options} original-code]
+	  (-> `(assoc ~'task :result ~original-code)
+	    ((wrap-if accumulate 
+	              `(let [result# ~code]
+	                 (update-in result# :results conj (:result result#)))))
+	    ((wrap-if while-clause 
+	              `(if ~while-clause 
+	                 ~code 
+	                 (assoc ~'task :status :complete))))
+	    ((wrap-if until-clause 
+	              `(let [result# ~code]
+                   (if ~until-clause 
+	                   result# 
+	                   (assoc result# :status :complete)))))
+ 	    ((wrap-if (number? repeat-value) 
+		            `(let [result# ~code
+		                   rep# (dec (:repeat result#))]
+		               (if (> rep# 0)
+		                 (assoc result# :repeat rep#)
+		                 (assoc result# :status :complete :repeat 0)))))
+ 	    ((wrap-if (not repeat-value) 
+					      `(let [result# ~code]
+					         (assoc result# :status :complete))))
+	    ((wrap-if sleep 
+	              `(let [result# ~code] 
+	                 (if (not (complete? ~'task)) (Thread/sleep ~sleep)) 
+	                 result#)))
+	    ((wrap-if true 
+	              `(fn [~'task] ~code)))))
+  
+  ([code] (build-task-function nil code)))
+
+(defmacro task 
+  "Creates a task containing the given code. Valid options are:
+  :repeat 
+     (Default) If left nil of false, the task will only execute once
+     If a numeric value is used, the task will repeat the given number of times. 
+     If any other true value is used, the task will repeat infinitely.
+
+  :result
+     An initial result value for the task. :result will be set to the value produced
+     by the task on each iteration.
+
+  :accumulate-results
+     If set to true, all results will be saved in the vector :results in the task.
+
+  :timeout
+     A number of milliseconds to run the task for. If the tomeout is reaced during
+     execution of the task, it will be allowed to complete.
+
+  :sleep
+     A number of milliseconds to sleep between successive executions of the task
+
+  :while
+     Code that will be called before each iteration with the task as an argument. 
+     If it returns true, then the task will complete.
+
+  :until
+     Code that will be evaluated after each iteration with the task as an argument. 
+     If it returns true then the task will complete.
+     "
+
+  ([options code]
+    (let [function (build-task-function options code)
+          options (merge options 
+                         {:source (str code)
+                          :options options})]
+      
+      `(task* (quote ~options) ~function)))
+  ([code] 
+    `(task nil ~code)))
+
+
+
+;; =====================================================================================
+;; Task management functions
+
+
+(defn ps 
+  ([]
+    (ps (vals @tasks)))
+  ([tasks]
+    (ps task-summary-fields tasks))
+  ([ks tasks]
+    (clojure.pprint/print-table ks (map task-summary tasks))))
+
+(defn stop [task]
+  (let [task (get-task task)
+        id (:id task)]
+    (dosync 
+      (alter tasks assoc id (assoc task :status :stopped)))))
+
+(defn await-result [task]
+  (let [task (get-task task)]
+    @(:promise task)))
+
+
+;; ==================================================================================
+;; Task execution
+
+(defn finish-task [task]
+  (let [task (assoc task :finish-time (time/now))
+        promise (:promise task)]
+    (if promise (deliver promise (:result task)))
+    task))
+
+(defn task-loop [task] 
+  (if (complete? task) 
+	  (finish-task task)
+    (let [id (:id task)
+          task (try 
+                 ((:function task) task) 
+                 (catch Throwable t (assoc task :status :error :error t)))
+          new-status (if (stopped? (get-task id)) :stopped (:status task))
+          task (assoc task :status new-status)]
+	    (dosync (alter tasks assoc id task))
+      (recur (@tasks id)))))
+	      
+
+(defn run-task [task]
+  (let [id (allocate-task-id)
+        task (merge task
+								    {:id id
+                     :status :started
+								     :start-time (time/now)})]
+	  (dosync (alter tasks assoc id task))
+    (future (task-loop task))
+    task))
+
+;; =========================================================================================
+;; Task launching macros
+
+(defmacro run 
+  "Create and run a new task. 
+
+  Calls task with the relevant options."
+  ([code]
+	 `(run-task (task ~code)))
+  
+  ([options code]
+	 `(run-task (task ~options ~code))))
+
 
---
+++ src/test/clojure/task/test_task.clj
@@ -0,0 +1,35 @@
+(ns task.test-task
+  (:use [clojure.test])
+  (:use [task.core]))
+
+
+(deftest test-function-generation
+  (testing "Task function generation"
+    (is (= (take 2 `(fn [~'task] (assoc ~'task :result 3))) 
+           (take 2 (build-task-function 3))))))
+
+(deftest test-running
+  (let [task (run "foobar")
+        id (:id task)]
+    (testing "Task running"
+      (is (= id (:id (get-task id)))))))
+
+(deftest test-results
+  (testing "Task results"
+    (is (= "foobar" (await-result (run "foobar"))))
+    (is (= 15 (await-result (run {:result 10 :repeat 5 } (inc (:result task))))))))
+
+(deftest test-while
+  (testing "Task results"
+    (is (= 15 
+           (await-result (run 
+                           {:result 0 
+                            :repeat 100
+                            :while (< (:result task) 15)} 
+                           (inc (:result task))))))
+    (is (= 10 
+           (await-result (run 
+                           {:result 0 
+                            :repeat 10
+                            :while (< (:result task) 15)} 
+                           (inc (:result task))))))))
